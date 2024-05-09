102ddc5987035121bcb96f46ee00c1ee7f294526
DreamZhongJu <zy18145326@163.com>
2024-05-09 22:08:02 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace Dashboard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button11 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(58)))), ((int)(((byte)(76)))));
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(87, 828);
+            this.panel1.TabIndex = 0;
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = global::Dashboard.Properties.Resources.箭头;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button6.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(0, 690);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(87, 69);
+            this.button6.TabIndex = 6;
+            this.button6.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = global::Dashboard.Properties.Resources.密码;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(0, 295);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(87, 69);
+            this.button5.TabIndex = 5;
+            this.button5.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = global::Dashboard.Properties.Resources.填写;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(0, 226);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 69);
+            this.button4.TabIndex = 4;
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::Dashboard.Properties.Resources.电源;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(0, 759);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 69);
+            this.button3.TabIndex = 3;
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::Dashboard.Properties.Resources.提示;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(0, 163);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 63);
+            this.button2.TabIndex = 2;
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Dashboard.Properties.Resources.图纸;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(0, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 63);
+            this.button1.TabIndex = 1;
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(87, 100);
+            this.panel2.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Dashboard.Properties.Resources.我的__2_;
+            this.pictureBox1.Location = new System.Drawing.Point(18, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 57);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.panel3.Location = new System.Drawing.Point(162, 100);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1017, 448);
+            this.panel3.TabIndex = 1;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(1068, 601);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(111, 41);
+            this.button7.TabIndex = 2;
+            this.button7.Text = "基础图形绘制";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(162, 601);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(111, 41);
+            this.button8.TabIndex = 3;
+            this.button8.Text = "渐变色字体";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(308, 601);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(111, 41);
+            this.button9.TabIndex = 4;
+            this.button9.Text = "艺术字";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(162, 574);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(257, 21);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(1068, 554);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(111, 41);
+            this.button10.TabIndex = 6;
+            this.button10.Text = "清空";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(425, 575);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(552, 569);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(81, 30);
+            this.button11.TabIndex = 8;
+            this.button11.Text = "加载字体";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(1606, 828);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button11;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Reflection;
+
+// 绘图
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+
+namespace Dashboard
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+
+        private static extern IntPtr CreateRoundRectRgn
+         (
+               int nLeftRect,
+               int nTopRect,
+               int nRightRect,
+               int nBottomRect,
+               int nWidthEllipse,
+               int nHeightEllipse
+
+         );
+
+        static Form1 _obj;
+        public static Form1 Instance
+        {
+            get {
+                if (_obj == null)
+                {
+                    _obj = new Form1();
+                }
+                return _obj;
+            }
+        }
+        public Button BackButton
+        {
+            get { return button3; }
+            set { button3 = value; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
+            panel1.Height = button1.Height;
+            panel1.Top = button1.Top;
+            panel1.Left = button1.Left;
+            button1.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Height = button1.Height;
+            panel1.Top = button1.Top;
+            panel1.Left = button1.Left;
+            button1.BackColor = Color.FromArgb(46,51,73);
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            panel1.Height = pictureBox1.Height;
+            panel1.Top = pictureBox1.Top;
+            panel1.Left = pictureBox1.Left;
+            pictureBox1.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            panel1.Height = button2.Height;
+            panel1.Top = button2.Top;
+            panel1.Left = button2.Left;
+            button2.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            panel1.Height = button4.Height;
+            panel1.Top = button4.Top;
+            panel1.Left = button4.Left;
+            button4.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            panel1.Height = button5.Height;
+            panel1.Top = button5.Top;
+            panel1.Left = button5.Left;
+            button5.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            panel1.Height = button6.Height;
+            panel1.Top = button6.Top;
+            panel1.Left = button6.Left;
+            button6.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // 绘图
+        // 使用pen绘制基础图形
+        private void button7_Click(object sender, EventArgs e)
+        {
+            panelClear();
+            Graphics g = panel3.CreateGraphics();
+            Pen pen = new Pen(Color.Blue, 3);
+
+            // 矩形
+            g.DrawRectangle(pen, new Rectangle(10, 10, 100, 50));
+            // 椭圆
+            g.DrawEllipse(pen, new Rectangle(120, 10, 100, 50));
+            // 线段
+            g.DrawLine(pen, new Point(10, 70), new Point(230, 70));
+        }
+
+        // 使用LinearGradientBrush实现渐变色字体
+        private void button8_Click(object sender, EventArgs e)
+        {
+            panelClear();
+            Graphics g = panel3.CreateGraphics();
+            string text = textBox1.Text.ToString();
+            Font font = new Font(selectedFontFamily, 20, FontStyle.Bold);
+            Rectangle textRect = new Rectangle(10, 90, 2000, 200);
+            LinearGradientBrush lgb = new LinearGradientBrush(textRect, Color.Red, Color.Blue, 45);
+            g.DrawString(text, font, lgb, textRect);
+        }
+
+        // 使用GraphicsPath实现艺术字
+        private void button9_Click(object sender, EventArgs e)
+        {
+            panelClear();
+            Graphics g = panel3.CreateGraphics();
+            GraphicsPath gp = new GraphicsPath(FillMode.Winding);
+            string text = textBox1.Text.ToString();
+            gp.AddString(
+                text, new FontFamily(selectedFontFamily), (int)FontStyle.Regular,
+                80, new PointF(10, 150), new StringFormat());
+
+            Brush brush = new LinearGradientBrush(new Rectangle(10, 150, 300, 100), Color.Green, Color.Yellow, 45);
+            g.DrawPath(Pens.Green, gp);
+            g.FillPath(brush, gp);
+        }
+
+        // 清空
+        private void button10_Click(object sender, EventArgs e)
+        {
+            panelClear();
+        }
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+        }
+        private void panelClear()
+        {
+            panel3.Invalidate();
+            panel3.Update();
+        }
+
+        // 选择字体
+        private string selectedFontFamily = "Arial";
+        private void button11_Click(object sender, EventArgs e)
+        {
+            // 加载所有字体到 comboBox1
+            InstalledFontCollection installedFonts = new InstalledFontCollection();
+            foreach (FontFamily font in installedFonts.Families)
+            {
+                comboBox1.Items.Add(font.Name);
+            }
+
+            // 订阅 comboBox1 的 SelectedIndexChanged 事件
+            comboBox1.SelectedIndexChanged += new EventHandler(comboBox1_SelectedIndexChanged);
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // 更新选中的字体
+            selectedFontFamily = comboBox1.SelectedItem.ToString();
+        }
+    }
+}
